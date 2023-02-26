7f5c369c5619b24517730f72f664c8afa6df6f84
DELL <DELL@DESKTOP-SVIQBSH>
2023-02-27 05:17:15 +0530

    first commit

---
+++ Trial/src/feb27/Hello.java
@@ -0,0 +1,13 @@
+package feb27;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		int a = 100;
+		int b = 200;
+		int c = a+b;
+		System.out.println("total is haha"+c);
+
+	}
+
+}
